a3b3f04fce9185f78232233180f280631a978272
Vincent582 <pasdeloupvincent@live.fr>
2019-11-29 14:50:15 +0900

    -Remove function not used in TaskRepository and ViewModel -Add Orchestrator to project

--- app/src/main/java/com/cleanup/todocmaster/repositories/TaskDataRepository.java
+++ app/src/main/java/com/cleanup/todocmaster/repositories/TaskDataRepository.java
@@ -11,20 +11,12 @@ public class TaskDataRepository {
 
     public TaskDataRepository(TaskDAO taskDAO) { mTaskDAO = taskDAO; }
 
-    public LiveData<List<Task>> getTasks(long projectId){
-        return this.mTaskDAO.getTasks(projectId);
-    }
-
     public LiveData<List<Task>> getAllTasks(){
         return this.mTaskDAO.getAllTasks();
     }
 
     public void insertTask(Task task){
         this.mTaskDAO.insertTask(task);
-    }
-
-    public void updateTask(Task task){
-        this.mTaskDAO.updateTask(task);
     }
 
     public void deleteTask(long taskId){
--- app/src/main/java/com/cleanup/todocmaster/ui/TaskViewModel.java
+++ app/src/main/java/com/cleanup/todocmaster/ui/TaskViewModel.java
@@ -2,8 +2,6 @@ package com.cleanup.todocmaster.ui;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.ViewModel;
-import android.support.annotation.Nullable;
-
 import com.cleanup.todocmaster.model.Project;
 import com.cleanup.todocmaster.model.Task;
 import com.cleanup.todocmaster.repositories.ProjectDataRepository;
@@ -19,34 +17,11 @@ public class TaskViewModel extends ViewModel {
     private final TaskDataRepository mTaskDataRepository;
     private final Executor mExecutor;
 
-    //Data
-    @Nullable
-    private LiveData<Project> currentProject;
-
     //CONSTRUCTOR
     public TaskViewModel(ProjectDataRepository projectDataRepository, TaskDataRepository taskDataRepository, Executor executor) {
         mProjectDataRepository = projectDataRepository;
         mTaskDataRepository = taskDataRepository;
         mExecutor = executor;
-    }
-
-    public void init(long projectId){
-        if (currentProject != null){
-            return;
-        }
-        currentProject = mProjectDataRepository.getProject(projectId);
-    }
-
-    public LiveData<Project> getProject(long projectId){
-        return this.currentProject;
-    }
-
-    public LiveData<List<Project>> getAllProjects(){
-        return this.mProjectDataRepository.getAllProjects();
-    }
-
-    public LiveData<List<Task>> getTasks(long projectId){
-        return this.mTaskDataRepository.getTasks(projectId);
     }
 
     public LiveData<List<Task>> getAllTasks(){
@@ -56,12 +31,6 @@ public class TaskViewModel extends ViewModel {
     public void insertTask(Task task){
         mExecutor.execute(()-> {
             this.mTaskDataRepository.insertTask(task);
-        });
-    }
-
-    public void updateTask(Task task){
-        mExecutor.execute(()->{
-            this.mTaskDataRepository.updateTask(task);
         });
     }
 
